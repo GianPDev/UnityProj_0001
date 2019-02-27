f9054d783999a778a38193f5973cecc91382fbf9
Gian P <gianpdev@gmail.com>
2019-02-27 16:00:24 +1100

    Attempted to add aiming thing.

---
+++ Assets/Scripts/Hello.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello from " + gameObject.name + " the " + gameObject.GetType());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+}
